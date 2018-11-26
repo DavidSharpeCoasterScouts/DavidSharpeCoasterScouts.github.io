6cdd7209fdcc9fae3c30028eab07cd9da5bddb96
DavidSharpeCoasterScouts <44170793+DavidSharpeCoasterScouts@users.noreply.github.com>
2018-11-26 10:20:50 -0600

    Add files via upload

---
+++ ISLT4360/PilotSite/contactform.php
@@ -0,0 +1,15 @@
+<?php 
+
+if (isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $subject=$_POST['subject'];
+    $mailFrom=$_POST['mail'];
+    $message=$_POST['message'];
+
+    $mailTo="sharpedavid5@gmail.com";
+    $headers="From: ".$mailFrom;
+    $txt="You have received an e-mail from ".$name.".\n\n".$message;
+
+    if (mail($mailTo, $subject, $txt, $headers)){echo "<h1>Sent Successfully! Thank You"."".$name.",We will contact you shortly!</h1>";} else {echo "Something went wrong! Please try again some other time"};
+    header("Location: CoasterScoutsContactMe.php?mailsend");
+}?>
